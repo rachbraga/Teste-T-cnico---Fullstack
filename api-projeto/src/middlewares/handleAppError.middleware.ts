08e68cb249db96b4acb65024ee0c4723bfaf56e0
Rachel Braga <rmbraga@me.com>
2022-09-26 13:20:00 -0300

    fix config

--- api-projeto/src/middlewares/handleAppError.middleware.ts
+++ api-projeto/src/middlewares/handleAppError.middleware.ts
@@ -12,7 +12,7 @@ const handleAppErrorMiddleware = (
       message: error.message,
     });
   }
-
+  console.log(error);
   return res.status(500).json({
     message: "Erro Interno do servidor.",
   });
